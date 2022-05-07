9d82423ff5facdd3e744c4475e184fa036ebcbea
rossica <rossica@users.noreply.github.com>
2022-05-07 13:39:10 -0700

    Initial commit.

---
+++ qsync.cpp
@@ -0,0 +1,11 @@
+#include "qsync.h"
+
+int main(
+    int argc,
+    char** argv)
+{
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+    printf("Hello world!\n");
+    return 0;
+}
