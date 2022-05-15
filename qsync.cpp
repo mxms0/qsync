b5a1bb3eafb3a037aba29b65199300ee8e639a6a
mxms <mxms@google.com>
2022-05-14 23:00:08 -0700

    Starting to do stuff in main

--- qsync.cpp
+++ qsync.cpp
@@ -1,11 +1,22 @@
 #include "qsync.h"
 
+void ParseArguments(QsyncSettings &Settings, int argc, char **argv) {
+    UNREFERENCED_PARAMETER(Settings);
+    UNREFERENCED_PARAMETER(argc);
+    UNREFERENCED_PARAMETER(argv);
+}
+
 int main(
     int argc,
     char** argv)
 {
-    UNREFERENCED_PARAMETER(argc);
-    UNREFERENCED_PARAMETER(argv);
     printf("Hello world!\n");
+
+    QsyncSettings Settings = { };
+    ParseArguments(Settings, argc, argv);
+    
+    const QUIC_API_TABLE *MsQuicApi = nullptr;
+    MASSERT(MsQuicOpen2(&MsQuicApi));
+    
     return 0;
 }
--- qsync.h
+++ qsync.h
@@ -7,3 +7,22 @@
 #ifndef WIN32
  #define UNREFERENCED_PARAMETER(x) (void)x
 #endif
+
+#define MASSERT(x) do { if (!(x)) { printf("Assert((%s) != true)", #x); } } while (0);
+
+enum QsyncPerspective {
+    Client = 0,
+    Server = 1,
+};
+
+typedef struct _Settings {
+    enum QsyncPerspective Perspective;
+    struct {
+        char *ServerAddress;
+        uint16_t ServerPort;
+    } ClientSettings;
+    struct {
+    
+    } ServerSettings;
+} QsyncSettings;
+
