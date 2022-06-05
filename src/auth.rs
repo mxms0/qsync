d47656cb6fcb541a5596ff44a938409185fb54f3
rossica <rossica@users.noreply.github.com>
2022-06-05 02:12:30 -0700

    Run rustfmt to clean up formatting.

--- src/auth.rs
+++ src/auth.rs
@@ -4,27 +4,33 @@
 //
 use openssl::asn1::Asn1Time;
 use openssl::bn;
+use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
 use openssl::pkcs12::Pkcs12;
-use openssl::pkey::{PKey, Private};
-use openssl::pkey::Id;
 use openssl::pkcs5::pbkdf2_hmac;
+use openssl::pkey::Id;
+use openssl::pkey::{PKey, Private};
 use openssl::rand::rand_bytes;
-use openssl::hash::MessageDigest;
-use openssl::x509::{X509Builder, X509NameBuilder, X509, X509NameRef};
+use openssl::x509::{X509Builder, X509NameBuilder, X509NameRef, X509};
 
 use std::cmp::Ordering;
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-const PBKDF_ITERATIONS:usize = 15000;
-const ED448_KEYLEN:usize = 57;
-const SIGNING_SALT_LENGTH:usize = 64;
-const CERT_VALIDITY_DAYS:u32 = 366;
-const FIVE_MINUTES:Duration = Duration::from_secs(300);
+const PBKDF_ITERATIONS: usize = 15000;
+const ED448_KEYLEN: usize = 57;
+const SIGNING_SALT_LENGTH: usize = 64;
+const CERT_VALIDITY_DAYS: u32 = 366;
+const FIVE_MINUTES: Duration = Duration::from_secs(300);
 
 fn generate_signing_key(password: &str, salt: &[u8]) -> Result<PKey<Private>, ()> {
     let mut signing_bytes: [u8; ED448_KEYLEN] = [0; ED448_KEYLEN];
-    if let Err(why) = pbkdf2_hmac(password.as_bytes(), salt, PBKDF_ITERATIONS, MessageDigest::sha512(), &mut signing_bytes) {
+    if let Err(why) = pbkdf2_hmac(
+        password.as_bytes(),
+        salt,
+        PBKDF_ITERATIONS,
+        MessageDigest::sha512(),
+        &mut signing_bytes,
+    ) {
         println!("PBKDF2 failed: {}", why);
         return Err(());
     }
@@ -33,12 +39,16 @@ fn generate_signing_key(password: &str, salt: &[u8]) -> Result<PKey<Private>, ()
         Err(why) => {
             println!("Failed to create signing key: {}", why);
             Err(())
-        },
-        Ok(key) => Ok(key)
+        }
+        Ok(key) => Ok(key),
     }
 }
 
-fn check_cert_name(actual_name: &X509NameRef, expected_name: &str, log_name: &str) -> Result<(), bool> {
+fn check_cert_name(
+    actual_name: &X509NameRef,
+    expected_name: &str,
+    log_name: &str,
+) -> Result<(), bool> {
     let mut entries = actual_name.entries();
     let checked;
     if let Some(entry) = entries.next() {
@@ -47,7 +57,12 @@ fn check_cert_name(actual_name: &X509NameRef, expected_name: &str, log_name: &st
                 checked = true;
             }
             _ => {
-                println!("{} name doesn't match! {:?} vs {}", log_name, entry.data(), expected_name);
+                println!(
+                    "{} name doesn't match! {:?} vs {}",
+                    log_name,
+                    entry.data(),
+                    expected_name
+                );
                 return Err(false);
             }
         }
@@ -66,21 +81,21 @@ fn check_cert_name(actual_name: &X509NameRef, expected_name: &str, log_name: &st
     Ok(())
 }
 
-    /// Generates a PKCS12 with a ED448 certificate and private key
-    /// signed with a ED448 key derived from the password.
-    /// The PKCS12 has an empty password, and null encryption, so it
-    /// MUST remain in memory and not be written to disk.
-    ///
-    /// # Examples
-    ///
-    /// Usage:
-    ///
-    /// ```
-    /// let pkcs12 = generate_auth_certificate("password", "app").unwrap();
-    /// let pkcs12 = Pkcs12::from_der(&pkcs12).unwrap();
-    /// let pkcs12 = pkcs12.parse("").unwrap();
-    /// verify_auth_certificate("password", "app", &pkcs12.cert).expect("Cert verification failed");
-    /// ```
+/// Generates a PKCS12 with a ED448 certificate and private key
+/// signed with a ED448 key derived from the password.
+/// The PKCS12 has an empty password, and null encryption, so it
+/// MUST remain in memory and not be written to disk.
+///
+/// # Examples
+///
+/// Usage:
+///
+/// ```
+/// let pkcs12 = generate_auth_certificate("password", "app").unwrap();
+/// let pkcs12 = Pkcs12::from_der(&pkcs12).unwrap();
+/// let pkcs12 = pkcs12.parse("").unwrap();
+/// verify_auth_certificate("password", "app", &pkcs12.cert).expect("Cert verification failed");
+/// ```
 pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<u8>, bool> {
     let mut salt_bytes: [u8; SIGNING_SALT_LENGTH] = [0; SIGNING_SALT_LENGTH];
 
@@ -94,7 +109,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create BigNum from salt bytes: {}", error);
             return Err(false);
         }
-        Ok(bn) => bn
+        Ok(bn) => bn,
     };
 
     let serial_number = match salt.to_asn1_integer() {
@@ -102,7 +117,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to convert BigNum to ASN1 integer: {}", error);
             return Err(false);
         }
-        Ok(sn) => sn
+        Ok(sn) => sn,
     };
 
     let mut cert_builder = match X509Builder::new() {
@@ -110,7 +125,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create new X509Builder: {}", error);
             return Err(false);
         }
-        Ok(x509) => x509
+        Ok(x509) => x509,
     };
 
     if let Err(error) = cert_builder.set_version(2) {
@@ -128,7 +143,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create NotAfter time: {}", err);
             return Err(false);
         }
-        Ok(val) => val
+        Ok(val) => val,
     };
 
     if let Err(error) = cert_builder.set_not_after(&not_after) {
@@ -142,7 +157,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to subtract 5 minutes from current time");
             return Err(false);
         }
-        Some(time) => time
+        Some(time) => time,
     };
 
     let not_before = match five_minutes_ago.duration_since(UNIX_EPOCH) {
@@ -150,7 +165,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("failed to get time since UNIX epoch: {}", why);
             return Err(false);
         }
-        Ok(time) => time
+        Ok(time) => time,
     };
 
     let not_before = not_before.as_secs() as i64;
@@ -160,7 +175,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create NotBefore time: {}", why);
             return Err(false);
         }
-        Ok(time) => time
+        Ok(time) => time,
     };
 
     if let Err(error) = cert_builder.set_not_before(&not_before) {
@@ -173,7 +188,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Keygen failed: {}", error);
             return Err(false);
         }
-        Ok(key) => key
+        Ok(key) => key,
     };
 
     if let Err(error) = cert_builder.set_pubkey(&private_key) {
@@ -186,7 +201,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create X509NameBuilder: {}", why);
             return Err(false);
         }
-        Ok(builder) => builder
+        Ok(builder) => builder,
     };
 
     if let Err(error) = name_builder.append_entry_by_text("CN", cert_name) {
@@ -200,7 +215,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
         println!("Failed to set subject name: {}", error);
         return Err(false);
     }
-    
+
     if let Err(error) = cert_builder.set_issuer_name(&name) {
         println!("Failed to set issuer name: {}", error);
         return Err(false);
@@ -208,7 +223,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
 
     let signing_key = match generate_signing_key(password, &salt_bytes) {
         Err(_) => return Err(false),
-        Ok(key) => key
+        Ok(key) => key,
     };
 
     if let Err(error) = cert_builder.sign(&signing_key, MessageDigest::null()) {
@@ -229,7 +244,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to create Pkcs12: {}", why);
             return Err(false);
         }
-        Ok(pfx) => pfx
+        Ok(pfx) => pfx,
     };
 
     match pkcs12.to_der() {
@@ -237,7 +252,7 @@ pub fn generate_auth_certificate(password: &str, cert_name: &str) -> Result<Vec<
             println!("Failed to export pkcs12: {}", why);
             Err(false)
         }
-        Ok(der) => Ok(der)
+        Ok(der) => Ok(der),
     }
 }
 
@@ -249,14 +264,15 @@ pub fn verify_auth_certificate(password: &str, cert_name: &str, cert: &X509) -> 
             println!("Failed to convert ASN SerialNumber to BIGNUM Salt: {}", why);
             return Err(false);
         }
-        Ok(sn) => sn
+        Ok(sn) => sn,
     };
 
     if serial_number.num_bytes() > SIGNING_SALT_LENGTH as i32 {
         println!(
             "Serial number is not correct size! {} vs {}",
             serial_number.num_bytes(),
-            SIGNING_SALT_LENGTH);
+            SIGNING_SALT_LENGTH
+        );
         return Err(false);
     }
 
@@ -270,12 +286,12 @@ pub fn verify_auth_certificate(password: &str, cert_name: &str, cert: &X509) -> 
             println!("BIGNUM conversion to binary failed: {}", why);
             return Err(false);
         }
-        Ok(sn) => sn
+        Ok(sn) => sn,
     };
 
     let signing_key = match generate_signing_key(password, &salt) {
         Err(_) => return Err(false),
-        Ok(key) => key
+        Ok(key) => key,
     };
 
     let now = match Asn1Time::days_from_now(0) {
@@ -283,7 +299,7 @@ pub fn verify_auth_certificate(password: &str, cert_name: &str, cert: &X509) -> 
             println!("Failed to create ASN1Time: {}", why);
             return Err(false);
         }
-        Ok(time) => time
+        Ok(time) => time,
     };
 
     match cert.not_before().compare(&now) {
@@ -297,7 +313,7 @@ pub fn verify_auth_certificate(password: &str, cert_name: &str, cert: &X509) -> 
                 return Err(false);
             }
             _ => {}
-        }
+        },
     }
 
     match cert.not_after().compare(&now) {
@@ -311,8 +327,7 @@ pub fn verify_auth_certificate(password: &str, cert_name: &str, cert: &X509) -> 
                 return Err(false);
             }
             _ => {}
-        }
-
+        },
     }
 
     check_cert_name(cert.issuer_name(), cert_name, "Issuer")?;
