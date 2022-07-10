e05bc73d215d137cde0c14532c13cafbe4bcd15c
mxms <mxms@google.com>
2022-07-10 03:38:20 -0700

    Add clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ pub mod files;
 
 use std::path::Path;
 
+use clap::Parser;
+
 use crate::files::walk_files;
 
+#[derive(Parser, Debug)]
+struct QsyncArguments {
+    /// Source to synchronize with
+    source: String,
+    /// Destination to synchronize to
+    destination: String,
+}
+
 fn main() {
-    let path = Path::new(".");
-    walk_files(path, |p| println!("{:?}", p)).expect("walk files failed!");
+    let args = QsyncArguments::parse();
+    println!("source: {:?}", args.source);
+    println!("destin: {:?}", args.destination);
+    walk_files(Path::new(&args.source), |p| println!("{:?}", p)).expect("walk files failed!");
+    walk_files(Path::new(&args.destination), |p| println!("{:?}", p)).expect("walk files failed!");
 }
