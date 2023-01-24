ef099c8cfeddcb26083fd1b4ccdce990c459d965
rossica <rossica@users.noreply.github.com>
2023-01-24 02:38:49 -0800

    Refactor VectorStream and disable getWriteBuffer()

--- vector_stream.h
+++ vector_stream.h
@@ -2,16 +2,28 @@
 
 class VectorStream: public kj::BufferedOutputStream {
 public:
-    explicit VectorStream(std::vector<uint8_t>& Vec);
+    explicit VectorStream(std::vector<uint8_t>& Vec)
+        : InnerVec(Vec), BytesWritten(0) {}
 
-    void write(const void* Buffer, size_t Size) override;
-    // Always writes the full size.  Throws exception on error.
+    void write(const void* Buffer, size_t Size)
+    {
+        if (Buffer == InnerVec.data() + BytesWritten) {
+            // std::cout << "buffer directly written " << Size << " bytes\n";
+            BytesWritten += Size;
+            // InnerVec.resize(BytesWritten);
+            // std:: cout << "buffer size " << InnerVec.size() << "\n";
+        } else {
+            // std::cout << "wrote " << Size << " bytes normally\n";
+            InnerVec.insert(InnerVec.end(), (uint8_t*)Buffer, (uint8_t*)Buffer + Size);
+            BytesWritten += Size;
+        }
+    }
 
-    inline
-    kj::ArrayPtr<kj::byte> getWriteBuffer() override { return kj::ArrayPtr<kj::byte>(InnerVec.data() + BytesWritten, available()); }
-
-    inline
-    size_t available() { return InnerVec.size() - BytesWritten; }
+    kj::ArrayPtr<kj::byte> getWriteBuffer()
+    {
+        // std::cout << "getWriteBuffer(" << BytesWritten << ") " << InnerVec.capacity() - BytesWritten <<"\n";
+        return kj::ArrayPtr<kj::byte>(InnerVec.data() + BytesWritten, 0ull); // disable this from being used by having a zero-length array
+    }
 private:
     std::vector<uint8_t>& InnerVec;
     size_t BytesWritten;
